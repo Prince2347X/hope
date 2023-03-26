80ecf40d4300b0ba9d3373189cea5d85bf23953a
Parth <www.parthsingh0808@gmail.com>
2023-03-26 16:24:52 +0530

    Initialized Hope App

---
+++ lib/users/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserLoginPage extends StatelessWidget {
+  const UserLoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
