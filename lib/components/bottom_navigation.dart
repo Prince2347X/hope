faa0fa0dfc2fb66bffc0a362bc74ed72408e6f16
Prince2347X <prince.raj29042004@gmail.com>
2023-05-14 09:33:14 +0530

    Created bottom navigation widget

---
+++ lib/components/bottom_navigation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldWithBottomNavBar extends StatelessWidget {
+  const ScaffoldWithBottomNavBar({required this.child, super.key});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
