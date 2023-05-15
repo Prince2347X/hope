d1731fbd8ef94d90575df23df4b4d6ba9762be90
Prince2347X <prince.raj29042004@gmail.com>
2023-05-15 17:40:51 +0530

    Added condition based initialLocation

--- lib/router/config.dart
+++ lib/router/config.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'package:hope/providers/auth_provider.dart';
 import 'package:hope/router/router.dart';
 
 final _key = GlobalKey<NavigatorState>(debugLabel: 'routerKey');
@@ -10,12 +11,13 @@ final _key = GlobalKey<NavigatorState>(debugLabel: 'routerKey');
 /// This simple provider caches our GoRouter.
 final routerProvider = Provider.autoDispose<GoRouter>((ref) {
   final notifier = ref.watch(routerNotifierProvider.notifier);
+  bool isLoggedIn = ref.watch(authStateProvider);
 
   return GoRouter(
     navigatorKey: _key,
     refreshListenable: notifier,
     debugLogDiagnostics: true,
-    initialLocation: '/login',
+    initialLocation: isLoggedIn ? '/hospitals' : '/onboarding',
     routes: notifier.routes,
   );
 });
